3a2877188a82d1821f5804fc752a77c5e5f259ef
KubaMatuszak <kuba.matuszak80@gmail.com>
2024-05-08 11:41:27 +0200

    Dodaj pliki projektów.

---
+++ EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email_Sender
+{
+	public class EmailSender : IEmailSender
+	{
+		public void SendMail(string email, string subject, string message)
+		{
+			var mail = "dotnettestmailsender@gmail.com";
+			var password = "";
+			var client = new SmtpClient("smtp.gmail.com", 587)
+			{
+				EnableSsl = true,
+				Credentials = new NetworkCredential(mail, password)
+			};
+			client.Send(new MailMessage(from: mail, to: email, subject, message));
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email_Sender
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEmailSender sender = new EmailSender();
+			Console.WriteLine("Wpisz adres email adresata");
+			var email = Console.ReadLine();
+			Console.WriteLine("Wpisz tytuł maila");
+			var subject = Console.ReadLine();
+			Console.WriteLine("Wpisz treść");
+			var msg = Console.ReadLine();
+			sender.SendMail(email, subject, msg);
+		}
+	}
+}
